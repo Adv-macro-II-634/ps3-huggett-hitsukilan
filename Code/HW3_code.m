fc81ad4f6d35254fb49d5cf4fb2d552e6be701dd
Danqing Lan <vikeylan@gmail.com>
2019-10-21 11:06:51 -0400

    submit

--- Code/HW3_code.m
+++ Code/HW3_code.m
@@ -3,7 +3,7 @@ clear;
 clc;
 
 % PARAMETERS
-beta = .9932; % discount factor 
+beta = .994; % discount factor 
 sigma = 1.5; % coefficient of risk aversion
 b = 0.5; % replacement ratio (unemployment benefits)
 y_s = [1, b]; % endowment in employment states
@@ -20,77 +20,76 @@ a = linspace(a_lo, a_hi, num_a); % asset (row) vector
 % INITIAL GUESS FOR q
 q_min = 0.98;
 q_max = 1.2;
-q_guess = (q_min + q_max) / 2;
 
-% ITERATE OVER ASSET PRICES
+
 aggsav = 1 ;
-while abs(aggsav) >= 0.01 
+
+while (abs(aggsav) >= 0.01)
+    
+    q_guess = (q_min + q_max) / 2;
     
     % CURRENT RETURN (UTILITY) FUNCTION
+    
     cons = bsxfun(@minus, a', q_guess * a);
+    
     cons = bsxfun(@plus, cons, permute(y_s, [1 3 2]));
+    
     ret = (cons .^ (1-sigma)) ./ (1 - sigma); % current period utility
+    ret(cons<0) = -Inf;
     
     % INITIAL VALUE FUNCTION GUESS
+    
     v_guess = zeros(2, num_a);
     
     % VALUE FUNCTION ITERATION
     v_tol = 1;
     
-    while v_tol >.0001
-        % CONSTRUCT RETURN + EXPECTED CONTINUATION VALUE
-        val_mat = ret + beta * repmat(permute((PI * v_guess),[3 2 1]), [num_a 1 1]);
-        % CHOOSE HIGHEST VALUE (ASSOCIATED WITH a' CHOICE)
-        [vfn, pol_index] = max(val_mat, [], 2);
+    while v_tol >.000001
         
-        v_tol = max(abs(permute(vfn, [3 1 2]) - v_guess));
-        v_tol = max(v_tol(:));
+        % CONSTRUCT RETURN + EXPECTED CONTINUATION VALUE
+        value_mat=ret+beta*repmat(permute((PI*v_guess),[3 2 1]), [num_a 1 1]);
+
+        % CHOOSE HIGHEST VALUE (ASSOCIATED WITH a' CHOICE)
+       [vfn, pol_indx] = max(value_mat, [], 2); %max for each row
+        
+       v_tol = max(abs(permute(vfn, [3 1 2])-v_guess));
+       v_tol = max(v_tol(:));
        
-        v_guess = permute(vfn, [3 1 2]);
-  
+       v_guess = permute(vfn, [3 1 2]);
     end
     
     % KEEP DECSISION RULE
-    
-    pol_indx = permute(pol_index,[3 1 2]);
+    pol_indx = permute(pol_indx, [3 1 2]);
     pol_fn = a(pol_indx);
     
     % SET UP INITITAL DISTRIBUTION
-    
     mu = zeros(2,num_a);
-    mu(:) = 1/(2 * num_a);
+    mu(:) = 1/(2*num_a);
     
-    dis = 1;
-    while dis > 0.00001
-        
+    dis=1;
+  while dis>0.0000001 
+      % ITERATE OVER DISTRIBUTIONS
+      MuNew = zeros(size(mu));
+     [emp_ind, a_ind, mass] = find(mu); % find non-zero indices
     
-        % ITERATE OVER DISTRIBUTIONS
-        MuNew = zeros(size(mu));
-        [emp_ind, a_ind, mass] = find(mu); % find non-zero indices
-       
-        for ii = 1:length(emp_ind)
-            
-        apr_ind = pol_indx(emp_ind(ii), a_ind(ii)); % which a prime does the policy fn prescribe?
+    for ii = 1:length(emp_ind)
+        apr_ind = pol_indx(emp_ind(ii),a_ind(ii)); % which a prime does the policy fn prescribe?
         
         MuNew(:, apr_ind) = MuNew(:, apr_ind) + ... % which mass of households goes to which exogenous state?
-            (PI(emp_ind(ii), :) * mass(ii))';
-        end
+            (PI(emp_ind(ii), :)*mass(ii))';
         
-     dis = max(max(abs(mu-MuNew)));
-     mu = MuNew;
     end
-    % market clearing 
-    
-    aggsav = mu(1,:) * pol_fn(1,:)' + mu(2,:) * pol_fn(2,:)';
-    
-    if aggsav > 0
-        q_min = q_guess;
-    else
-        q_max = q_guess;
-    end
-        
+    dis = max(max(abs(mu-MuNew)));
+    mu = MuNew;
+  end
+   %Market clears
+   aggsav = mu(1,:)*pol_fn(1,:)'+mu(2,:)*pol_fn(2,:)';
+   if aggsav>0
+       q_min = q_guess;
+   else
+       q_max = q_guess;
+   end
 end
-
 
 % Question 3 Gini coefficient and lorenz curve
 
@@ -102,20 +101,36 @@ nonnegwealth = wealth;
 nonnegwealth(nonnegwealth<0) = 0;
 
 figure(1)
-subtitle('Lorenz Curve for Wealth and Earnings')
-
 subplot(1,2,1)
-gini_wealth = gini(pop, nonnegwealth, true);
-title('Lorenz Curve for Wealth',num2str(gini_wealth),'FontSize',18);
+gini_wealth = gini(popu, nonnegwealth, true);
+title({'Lorenz Curve for Wealth','Gini coefficient = ',num2str(gini_wealth)});
 
 subplot(1,2,2)
-gini_earnings = gini(pop, earning, true);
-title('Lorenze Curve for Earning',num2str(gini_earnings),'FontSize',18);
+gini_earnings = gini(popu, earning, true);
+title({'Lorenze Curve for Earning','Gini coefficient = ',num2str(gini_earnings)});
 
 % Extra Credit
 
 PI_LR = PI^1000;
-C = PI_LR(1,:) * y_s';
-W_FB = (c^(1-sigma))/(1-sigma)/(1-beta);
+C_bar = PI_LR(1,:) * y_s';
+W_FB = (C_bar^(1-sigma))/(1-sigma)/(1-beta);
+lambda = (v_guess.^(-1).* W_FB).^(1/(1-sigma))-1;
+Hh = sum(sum((lambda >0) .* mu));
+WFG = sum(sum(lambda .* mu));
+
+figure(2)
+
+plot(a,lambda(1,:),'DisplayName','Employed', 'LineWidth',2);
+hold on;
+plot(a,lambda(2,:),'DisplayName','Unemployed','LineWidth',2);
+hold off;
+title('Consumption Equivalent','FontSize',18);
+lgd = legend;
+lgd.Location ='Northeast';
+lgd.FontSize = 14;
+
+
+
+
 
 
